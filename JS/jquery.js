a5ef6ab2583aeaf3ae02da9263f32cc55a6c880e
Zanshin <thomasmperrot@gmail.com>
2017-01-15 19:07:19 +0100

    Add files via upload

---
+++ JS/jquery.js
@@ -0,0 +1,12 @@
+$(window).load(function () {
+    $('.flexslider').flexslider({
+        animation: "slide"
+        , animationLoop: true
+        , slideshow: true
+        , minItems: 3
+        , slideshowSpeed: 4000
+        , pauseOnHover: true
+        , controlNav: false
+        , directionNav: false
+    });
+});
